0dd52460165c5e3ce2e0fc3f965aa5c2f8c205d1
jbpratt <jbpratt78@gmail.com>
2018-11-19 10:05:10 -0600

    inital commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type streamList struct {
+	Streams      int `json:"streams"`
+	TotalViewers int `json:"total_viewers"`
+}
+
+func main() {
+	url := "https://angelthump.com/api"
+
+	angelthumpClient := http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "chibidesti")
+
+	res, getErr := angelthumpClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	streams := streamList{}
+	jsonErr := json.Unmarshal(body, &streams)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	fmt.Println("Total viewer count: ", streams.TotalViewers)
+	fmt.Println("Stream count: ", streams.Streams)
+
+}
